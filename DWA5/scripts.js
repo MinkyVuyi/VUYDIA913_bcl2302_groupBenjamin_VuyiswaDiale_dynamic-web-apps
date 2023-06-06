b101fdfa13516bfac2ad7e81bd8c1926a5f11ea0
MinkyVuyi <dialevuyi@gmail.com>
2023-06-06 14:33:39 +0200

    new

---
+++ DWA5/scripts.js
@@ -0,0 +1,61 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  result.innerText = dividend / divider;
+});
+
+// Assuming you have an HTML element with the attribute data-form="data-form" for the form,
+// an HTML element with the attribute data-result="data-result" to display the result,
+// and the JavaScript code is in a file named "scripts.js" in the same directory as the HTML file.
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("[data-form='data-form']");
+    const resultElement = document.querySelector("[data-result='data-result']");
+  
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+  
+      const dividendInput = document.querySelector("input[name='dividend']");
+      const dividerInput = document.querySelector("input[name='divider']");
+  
+      const dividend = parseFloat(dividendInput.value);
+      const divider = parseFloat(dividerInput.value);
+  
+      if (isNaN(dividend) || isNaN(divider)) {
+        replaceScreenWithError("Something critical went wrong. Please reload the page");
+        console.error("Invalid input provided");
+        return;
+      }
+  
+      if (divider < 0) {
+        displayError("Division not performed. Invalid number provided. Try again");
+        console.error("Division by a negative number");
+        return;
+      }
+  
+      if (!dividendInput.value || !dividerInput.value) {
+        displayError("Division not performed. Both values are required in inputs. Try again");
+        console.error("Missing input values");
+        return;
+      }
+  
+      const result = Math.floor(dividend / divider);
+      resultElement.textContent = result.toString();
+    });
+  
+    function displayError(message) {
+      resultElement.textContent = message;
+    }
+  
+    function replaceScreenWithError(errorMessage) {
+      document.body.innerHTML = `<h1>Error</h1><p>${errorMessage}</p>`;
+      console.error(errorMessage);
+    }
+  });
+  
