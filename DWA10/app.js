81c52b42d691fcc31ff048eb77e919dcf68c314c
MinkyVuyi <dialevuyi@gmail.com>
2023-06-22 06:04:47 +0200

    tally

--- DWA10/app.js
+++ DWA10/app.js
@@ -1,38 +1,43 @@
-const updateCounterValue = () => {
-    const number = document.querySelector('sl-input[data-key="number"]');
-    if (number) {
-  
-    }
-  };
-  
-  const subtract = document.querySelector('sl-button[data-key="subtract"]');
-  const add = document.querySelector('sl-button[data-key="add"]');
-  const reset = document.querySelector('sl-button[data-key="reset"]');
-  
-  const subtractHandler = () => {
-    const number = document.querySelector('sl-input[data-key="number"]');
-    if (number) {
-      const newValue = parseInt(number.value) - 1;
-      number.value = newValue;
-    }
-  };
-  
-  const addHandler = () => {
-    const number = document.querySelector('sl-input[data-key="number"]');
-    if (number) {
-      const newValue = parseInt(number.value) + 1;
-      number.value = newValue;
-    }
-  };
-  
-  const resetHandler = () => {
-    const number = document.querySelector('sl-input[data-key="number"]');
-    if (number) {
-      number.value = 0;
-    }
-  };
-  
-  subtract.addEventListener('click', subtractHandler);
-  add.addEventListener('click', addHandler);
-  reset.addEventListener('click', resetHandler);
-  
+const MAX_NUMBER = 30;
+const MIN_NUMBER = -5;
+
+const number = document.querySelector('sl-input[data-key="number"]');
+const subtract = document.querySelector('sl-button[data-key="subtract"]');
+const add = document.querySelector('sl-button[data-key="add"]');
+const reset = document.querySelector('sl-button[data-key="reset"]');
+
+const subtractHandler = () => {
+  const newValue = parseInt(number.value) - 1;
+  number.value = newValue;
+
+  if (add.disabled === true) {
+    add.disabled = false;
+  }
+
+  if (newValue <= MIN_NUMBER) {
+    subtract.disabled = true;
+  }
+};
+
+const addHandler = () => {
+  const newValue = parseInt(number.value) + 1;
+  number.value = newValue;
+
+  if (subtract.disabled === true) {
+    subtract.disabled = false;
+  }
+
+  if (newValue >= MAX_NUMBER) {
+    add.disabled = true;
+  }
+};
+
+const resetHandler = () => {
+  number.value = 0;
+  subtract.disabled = true;
+  add.disabled = false;
+};
+
+subtract.addEventListener('click', subtractHandler);
+add.addEventListener('click', addHandler);
+reset.addEventListener('click', resetHandler);
