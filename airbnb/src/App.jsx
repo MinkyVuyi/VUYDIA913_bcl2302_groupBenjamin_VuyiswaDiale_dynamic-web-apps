30bc6a6af0179160d9f954bd6e6ac3bde7b9b48b
MinkyVuyi <dialevuyi@gmail.com>
2023-07-08 18:59:57 +0200

    changes

---
+++ airbnb/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+            
+  return (
+      <div>
+          <Navbar />
+          <Hero />
+          <Card />
+      </div>
+  )
+}
---
+++ info-site/src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/Main.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+export default function App() {
+  return (
+      <div className="container">
+          <Navbar />
+          <Main />
+      </div>
+  )
+}
