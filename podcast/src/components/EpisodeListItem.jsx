4f8fe722c477ea03d33c3369c8fdd74f7e57235b
MinkyVuyi <dialevuyi@gmail.com>
2023-08-02 09:42:11 +0200

    doneish

--- podcast/src/components/EpisodeListItem.jsx
+++ podcast/src/components/EpisodeListItem.jsx
@@ -1,31 +1,16 @@
-// import React from 'react';
-
-// const EpisodeListItem = ({ episode }) => {
-//   return (
-//     <div className="episode-list-item">
-//       <div>
-//         <strong>{episode.title}</strong>
-//       </div>
-//       <div>Season {episode.season} - Episode {episode.number}</div>
-//       <div>{episode.description}</div>
-//       {/* You can add more details or UI elements as needed */}
-//     </div>
-//   );
-// };
-
-// export default EpisodeListItem;
-
 import React from 'react';
-import EpisodeListItem from './EpisodeListItem';
 
-const EpisodeList = ({ episodes }) => {
+const EpisodeListItem = ({ episode }) => {
   return (
-    <div className="episode-list">
-      {episodes.map((episode) => (
-        <EpisodeListItem key={episode.id} episode={episode} />
-      ))}
+    <div className="episode-list-item">
+      <div>
+        <strong>{episode.title}</strong>
+      </div>
+      <div>Season {episode.season} - Episode {episode.number}</div>
+      <div>{episode.description}</div>
+      {/* You can add more details or UI elements as needed */}
     </div>
   );
 };
 
-export default EpisodeList;
+export default EpisodeListItem;
