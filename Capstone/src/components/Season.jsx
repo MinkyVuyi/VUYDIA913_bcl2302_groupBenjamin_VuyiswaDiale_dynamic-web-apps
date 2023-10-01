ee7ddccf6a406f78f221c68bdf028cbe0c6a32b9
MinkyVuyi <dialevuyi@gmail.com>
2023-10-01 21:37:14 +0200

    Update Season.jsx

--- Capstone/src/components/Season.jsx
+++ Capstone/src/components/Season.jsx
@@ -1,58 +1,35 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from 'react';
+import EpisodeButton from './EpisodeButton'; // Import the EpisodeButton component
 
-const Seasons = () => {
-  const { id } = useParams();
-  const [seasons, setSeasons] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    axios
-      .get(`https://podcast-api.netlify.app/id/${id}`)
-      .then((response) => {
-        setSeasons(response.data.seasons);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching season data:', error);
-        setLoading(false);
-      });
-  }, [id]);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+const Season = ({ season, selectedSeason, onSeasonClick, onFavoriteClick }) => {
+  const handleSeasonClick = (season) => {
+    onSeasonClick(season);
+  };
 
   return (
-    <div className="seasons-container">
-      <h2>Seasons</h2>
-      <ul>
-        {seasons.map((season, index) => (
-          <li key={index}>
-            <h3>{season.title}</h3>
-            <img src={season.image} className="season-image" alt={season.title} />
-            <ul>
-              {Array.isArray(season.episodes) ? (
-                season.episodes.map((episode, episodeIndex) => (
-                  <li key={episodeIndex}>
-                    <h4>{episode.title}</h4>
-                    <p>{episode.description}</p>
-                    <audio controls>
-                      <source src={episode.file} type="audio/mpeg" />
-                      Your browser does not support the audio element.
-                    </audio>
-                  </li>
-                ))
-              ) : (
-                <li>No episodes found.</li>
-              )}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <button
+        className={`season-button${selectedSeason === season ? ' selected' : ''}`}
+        onClick={() => handleSeasonClick(season)}
+      >
+        <h2>Season: {season.season}</h2>
+        <img src={season.image} className="show-image" alt={season.title} />
+      </button>
+      {selectedSeason === season && Array.isArray(season.episodes) && (
+        <ul className={`episodes-list${selectedSeason === season ? ' show-episodes' : ''}`}>
+          {season.episodes.map((episode, episodeIndex) => (
+            <li key={episodeIndex}>
+              <EpisodeButton
+                episode={episode}
+                onEpisodeClick={() => {}}
+                onFavoriteClick={onFavoriteClick}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default Seasons;
+export default Season;
